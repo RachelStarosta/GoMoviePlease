0de44d8089076d092a310999ef2452d7374fd8ea
Rachel Starosta <raychelstarr21@gmail.com>
2023-05-12 13:57:10 -0400

    First commit
    finally connected to backend

---
+++ src/Components/Movie.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+
+const Movie = () => {
+
+    const [movie, setMovies] = useState({});
+    /**
+     * We need to get the value of the id from the URL 
+     * react router makes this quite easy
+     */
+    let { id } = useParams();
+
+    useEffect(() => {
+        const headers = new Headers();
+        headers.append("Content-Type", "application/json");
+        const requestOptions = {
+            method: "GET",
+            headers: headers,
+        }
+
+        fetch( `/movies/${id}` , requestOptions) 
+            .then((response) => response.json())
+            .then((data) => {
+                setMovies(data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+                
+
+       
+    }, [id]);
+
+    return(
+        
+        <div>
+            <h2> Movie: {movie.title}</h2>
+            <small> <em> {movie.release_date}, {movie.runtime} minutes, Rated {movie.mpaa_rating} </em></small>
+            <hr/>
+            
+            <p> {movie.description} </p>
+        </div>
+        
+    )
+
+    }
+export default Movie;
